65f9c227047d892d2fa9a7c80a01d2afd82c5689
alejoacevedo <ale.acevedo@live.com.ar>
2018-06-26 19:30:35 -0300

    Subo todo

--- OptimalK.py
+++ OptimalK.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import adjusted_rand_score
 import pandas as pd
+from sklearn import preprocessing
 
 stopwords = "el la los les las de del a ante con en para por y o u tu te ti le que al ha un han lo su una estas esto este es tras suya a acá ahí ajena ajenas ajeno ajenos al algo algún alguna algunas alguno algunos allá alli allí ambos ampleamos ante antes aquel aquella aquellas aquello aquellos aqui aquí arriba  asi atras aun aunque bajo bastante bien cabe cada casi cierta ciertas cierto ciertos como cómo con conmigo conseguimos conseguir consigo consigue consiguen consigues contigo contra cual cuales cualquier cualquiera cualquieras cuancuán cuando cuanta cuánta cuantas cuántas cuanto cuánto cuantos cuántos de dejar del demás demas demasiada demasiadas demasiado demasiados  dentro desde donde dos el él ella ellas  ello ellos empleais emplean emplear empleas empleo en encima entonces entre era eramos eran eras eres es esa esas ese eso esos esta estaba estado estais estamos estan estar estas este esto estos estoy etc fin fue fueron fui fuimos gueno ha hace haceis hacemos hacen hacer haces hacia"
 stopwords = stopwords + "haago hasta incluso intenta intentais intentamos intentan intentar intentas intento ir jamás junto juntos la largo las lo los mas más me menos mi mía mia mias mientras mio mío mios mis misma mismas mismo mismos modo mucha muchas muchísima muchísimas muchísimo muchísimos mucho muchos muy nada ni ningun ninguna ningunas ninguno ningunos no nos nosotras nosotros nuestra nuestras nuestro nuestros nunca os otra otras otro otros para parecer pero poca pocas poco pocos podeis podemos poder podria podriais podriamos podrian podrias por por qué porque primero primero desde puede pueden puedo pues que qué querer quien quién quienes quienes quiera quienquiera quiza quizas sabe sabeis sabemos saben saber sabes se segun ser si sí siempre siendo sin sín sino so sobre sois solamente solo somos soy sr sra sres esta su sus suya suyas suyo suyos tal tales también tambien tampoco tan tanta tantas tanto tantos te teneis tenemos tener tengo ti tiempo tiene tienen toda todas todo todos tomar trabaja"
@@ -17,28 +18,28 @@ stopwords = stopwords +  "trabajais trabajamos trabajan trabajar trabajas trabaj
 stopwords = stopwords.split(' ')
 
 
-detalles = pd.read_csv('Sources/fiuba_6_avisos_detalle_limpio.csv')
-documents = detalles['descripcion']
-documents = documents.fillna('')
+df = pd.read_csv('Sources/test_kmeans_post.csv')
+le = preprocessing.LabelEncoder()
+for col in df.columns:
+    if not df[col].dtype == 'O': continue
+    df[col] = df[col].fillna('na')
+    le.fit(df[col])
+    df[col] = le.transform(df[col])
 
-vectorizer = TfidfVectorizer(stop_words=stopwords)
-X = vectorizer.fit_transform(documents)
-
-
+df.fillna(-1)
 
 # k means determine k
-distortions = []
-K = range(100,1000,100)
+dispercion = []
+K = range(10,300,10)
 for k in K:
     print(k)
     kmeanModel = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=1, n_jobs=-1)
-    kmeanModel.fit(X)
-    distortions.append(sum(np.min(cdist(X.toarray(), kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+    dispercion.append(kmeanModel.fit(df).inertia_/df.shape[0])
 
 # Plot the elbow
-plt.plot(K, distortions, 'bx-')
+plt.plot(K, dispercion, 'bx-')
 plt.xlabel('k')
 plt.ylabel('Distortion')
 plt.title('The Elbow Method showing the optimal k')
-plt.savefig('K_Test.png')
+plt.show()
 
